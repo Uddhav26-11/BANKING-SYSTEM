c128bfaf1c0647b75156d2cd9e06a35f19f1fbc9
Uddhav26-11 <uddhavchourasiya123@gmail.com>
2026-06-17 16:23:56 +0530

    Fix ratelimiter imports

--- banking-frontend/src/api/axios.js
+++ banking-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://banking-system-4-z4iq.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
